5e44d07d65ba5b2213ceee4c1bd51656412cea7d
Julia Ashaeva <julia.ashaeva@7bits.it>
2016-02-10 17:04:33 +0600

    1 and 2 tests works

--- spec/commons/AdminPage.rb
+++ spec/commons/AdminPage.rb
@@ -27,12 +27,26 @@ class AdminPage
   end
 
   #delete promo with number num
-  def delete(num)
+  def delete_by_n(num)
     self.delete_elements[num].click
     self.alert_element.when_present
     ok    
   end
-  
+
+  def delete(promo_name)
+    i=0
+      self.promo_elements.each do |prom|
+        # prom.when_present
+        if (prom.value == promo_name)
+          self.delete_elements[i-1].click
+          self.alert_element.when_present
+          ok
+          break
+        end
+        i = i+1
+      end
+  end
+
   #edit some promo
   def edit(num)
     self.edit_elements[num].click
@@ -42,4 +56,15 @@ class AdminPage
     self.promo_elements.size-1
   end
   
+  def find(promo_name)
+    exists = false    
+    self.promo_elements.each do |prom|      
+      if prom.value == promo_name          
+        exists = true
+        break
+      end
+    end
+    exists
+  end
+
 end
--- spec/device_feature.rb
+++ spec/device_feature.rb
@@ -18,7 +18,7 @@ describe "AdminPage" do
     end
 
     it "promo's name can not be empty"   do
-      page.add ""         
+      page.add("")     
       not_first = false
       page.promo_elements.each do |prom|        
         expect(prom.value != "").to be_truthy if (not_first && prom.visible?) 
@@ -27,42 +27,19 @@ describe "AdminPage" do
     end   
 
     it "user can add promo" do      
-      page.add(promo_name)      
-     
-      # page.save_screenshot "add_1_promo.png" if page.has_expected_element?
-      # binding.pry       
-      page = AdminPage.new(browser, true)     
-      # page.add(promo_name)
-      # page = AdminPage.new(browser, true)
-      # page.save_screenshot "add_2_promo.png" if page.has_expected_element?
-      # page.alert_element.when_present 
-      # expect(page.alert.include?  "Промо #{promo_name} уже существует").to be_truthy
-      # page.save_screenshot "add_1_promo.png"   if page.has_expected_element?
-      exists=false
-      # page.save_screenshot "add_2_promo.png" if page.has_expected_element?
-      page.promo_elements.each do |prom|
-        if prom.value == promo_name          
-          exists = true
-          break
-        end
-      end
-      expect(exists).to be_truthy
-
-
-      # page.ok      
+      page.add(promo_name)           
+      page = AdminPage.new(browser, true)           
+      expect(page.find(promo_name)).to be_truthy        
     end
 
+
     after (:all) do
-      page = AdminPage.new(browser, true)
-      page.has_expected_element?
-      i=0
-      page.promo_elements.each do |prom|
-        if (prom.value == promo_name)
-          page.delete(i-1) 
-          break
-        end
-        i = i+1
-      end
+      page = AdminPage.new(browser, true)      
+      expect(page.has_expected_element?).to be_truthy      
+      page.delete(promo_name)                 
+      page = AdminPage.new(browser, true)      
+      expect(page.has_expected_element?).to be_truthy      
+      expect(page.find(promo_name)).to be_falsey       
     end
   end  
 
